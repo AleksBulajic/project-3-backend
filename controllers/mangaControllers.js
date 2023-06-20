3efaf669555c0864a1b4bfe27ad75ced49d1f010
kevininga-ga <kevininga813@gmail.com>
2023-06-20 17:30:54 -0400

    changed manga by title controller to allow for front end usability

--- controllers/mangaControllers.js
+++ controllers/mangaControllers.js
@@ -1,9 +1,9 @@
-import Manga from '../models/manga.js';
+import Manga from "../models/manga.js";
 
 // Get all mangas
 export const getMangas = async (req, res) => {
   try {
-    if(!req.header.cookie){
+    if (!req.header.cookie) {
       const mangas = await Manga.find({});
       res.status(200).json(mangas);
       console.log(`THESE ARE THE MANGAS FROM MANGA CONTROLLERS: ${mangas}`);
@@ -15,32 +15,38 @@ export const getMangas = async (req, res) => {
 
 //Find a single manga by id
 export const getMangaById = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const manga = await Manga.findById(id);
-  
-      if (!manga) {
-        return res.status(404).json({ error: 'Manga was not found' });
-      }
-  
-      res.status(200).json(manga);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
+  try {
+    const { id } = req.params;
+    const manga = await Manga.findById(id);
+
+    if (!manga) {
+      return res.status(404).json({ error: "Manga was not found" });
     }
+
+    res.status(200).json(manga);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 // Get a manga by title
 export const getMangaByTitle = async (req, res) => {
-    try {
-      const { title } = req.params;
-      const manga = await Manga.findOne({ title: title }); 
-      if (!manga) {
-        return res.status(404).json({ error: 'Manga with that title not found' });
-      }
-      res.status(200).json(manga);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-};
+  try {
+    const { title } = req.params;
 
-//need to keep working on the full CRUD
+    // Use a regular expression for a case-insensitive search
+    // and find all manga titles that start with the searched title.
+    const manga = await Manga.find({ title: new RegExp("^" + title, "i") });
+
+    if (manga.length === 0) {
+      return res.status(404).json({ error: "No mangas found with that title" });
+    }
+
+    // Sort the results in ascending order based on the title.
+    manga.sort((a, b) => a.title.localeCompare(b.title));
+
+    res.status(200).json(manga);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
