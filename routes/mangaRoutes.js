219de90d064cb6450a307b00f784929d4530cdec
kevininga-ga <kevininga813@gmail.com>
2023-06-23 22:28:29 -0400

    added pages for vagabond, 15 out of 50 done

--- routes/mangaRoutes.js
+++ routes/mangaRoutes.js
@@ -5,12 +5,12 @@ import verifyAuth from "../middleware/verifyAuth.js";
 const router = Router();
 
 //retrieve a list of all mangas
-router.get("/all", verifyAuth, controllers.getMangas);
+router.get("/all", controllers.getMangas);
 
 //retrieve a specific manga by title
-router.get("/title/:title", verifyAuth, controllers.getMangaByTitle);
+router.get("/title/:title", controllers.getMangaByTitle);
 
 //retrieve a specific manga by id
-router.get("/id/:id", verifyAuth, controllers.getMangaById);
+router.get("/id/:id", controllers.getMangaById);
 
 export default router;
